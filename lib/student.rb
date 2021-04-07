fcf31e7ebe999feadeddb98c89a15a3e70113f31
Janis Lebron <janislebron@gmail.com>
2021-04-07 01:47:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,10 @@ class Student
   def save
     sql <<-SQL
     INSERT INTO students(name, grade)
+    VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 
   def self.create(name, grade)
